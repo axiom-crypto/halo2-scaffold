fc5b7a9c0b422346460df9ccd823b217d138275a
PatStiles <pdstiles78@gmail.com>
2023-06-03 21:42:47 -0400

    fix: oops forgot a variable

--- examples/var_len_keccak.rs
+++ examples/var_len_keccak.rs
@@ -12,6 +12,7 @@ use serde::{Deserialize, Serialize};
 pub struct CircuitInput {
     pub bytes31: String, // 1 field elements, but as strings for easier deserialization
     pub max_len: u64, // maximum length in bytes of the input string
+    pub min_len: usize, // minimum length in bytes of the input string
 }
 
 // Like the fixed length example we supply `bytes31` representing `31` bytes. We decompose the field eleemnt in big endian into 31 bytes.
@@ -29,7 +30,7 @@ fn compute_var_len_keccak<F: Field>(
 
     // load the input value as a witness
     let x = ctx.load_witness(F::from_str_vartime(&inp.bytes31).unwrap());
-    // We supply the `max_len` as a witness as we need to range check it to the size of the input before performing the keccak computation
+    // We supply the `max_len` and `min_len` as a witnesses as we need to range check it to the size of the input before performing the keccak computation
     let max_len = ctx.load_witness(F::from(inp.max_len));
     make_public.push(x);
 
@@ -40,10 +41,10 @@ fn compute_var_len_keccak<F: Field>(
     // Decompose x into 31 bytes, using a utility function from axiom-eth
     let bytes = uint_to_bytes_be(ctx, range, &x, 31);
 
-    // To perform variable length keccak we supply the range of bytes we want to hash over (min_len..max_len)
+    // To perform variable length keccak we supply the range of bytes we want to hash over [min_len..max_len]
     // We perform a range check on this range behind the scenes to confirm we are hashing over a valid range of the input
     // For faster witness generation, we can supply the raw bytes of the input
-    let hash_idx = keccak.keccak_var_len(ctx, range, bytes, Some(inp.bytes31.into_bytes()), max_len, 0);
+    let hash_idx = keccak.keccak_var_len(ctx, range, bytes, Some(inp.bytes31.into_bytes()), max_len, inp.min_len);
 
     // Fetch the value of the variable length keccak hash from the "keccak table"
     let out_bytes32 = keccak.var_len_queries[hash_idx].output_assigned.clone();
